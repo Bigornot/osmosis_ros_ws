81399e11772a74ddf6b917f865246b2c37b30784
Antonin Messioux <amessioux@cabernet.laas.fr>
2019-06-24 11:02:03 +0200

    debug

--- src/osmosis_ros/src/DM3_WrongCommand.cpp
+++ src/osmosis_ros/src/DM3_WrongCommand.cpp
@@ -7,12 +7,14 @@ DM3_WrongCommand::DM3_WrongCommand()
 {
 	cmd_sub_  = nh_.subscribe("summit_xl_a/robotnik_base_control/cmd_vel", 1, &DM3_WrongCommand::DM3_WrongCommandCallback, this);
 	DM3_pub_ = nh_.advertise<std_msgs::Bool>("DM3_WrongCommand", 10);
+	cmd_linear_x_max_=0;
+	cmd_angular_z_max_=0;
 }
 
 //compute detection out of zone
 bool DM3_WrongCommand::detect()
 {
-	if (cmd_.linear.x > cmd_linear_x_max_ || cmd_.angular.z > cmd_angular_z_max_)
+	if (cmd_.linear.x > cmd_linear_x_max_ || cmd_.angular.z > cmd_angular_z_max_ || -cmd_.linear.x < -cmd_linear_x_max_ || -cmd_.angular.z < -cmd_angular_z_max_)
 	{
 		std::cout << "Wrong command for the robot !!" << '\n';
 		return true;
@@ -30,15 +32,16 @@ void DM3_WrongCommand::DM3_WrongCommandCallback(const geometry_msgs::Twist & cmd
 
 void DM3_WrongCommand::pub_to_FTM(std_msgs::Bool donnee)
 {
+	bool d = donnee.data;
 	DM3_pub_.publish(donnee);
-	std::cout << "Publication de DM3 : WrongCommand" << '\n';
+	std::cout << "Publication de DM3 : WrongCommand" << std::endl;
+	std::cout << "etat =" << d << std::endl;
 }
 
 int main(int argc, char** argv)
 {
 	//init the ROS node
 	ros::init(argc, argv, "DM3_WrongCommand_node");
-
 	DetectionModule* myDM3_WrongCommand=new DM3_WrongCommand();
 	myDM3_WrongCommand->run();
 }
