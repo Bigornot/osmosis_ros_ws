cc6c8322f7049c73fdd3ef0c6ceafa0cda104f08
Antonin Messioux <amessioux@cabernet.laas.fr>
2019-07-17 18:07:44 +0200

    injection de faute sur DM1, rajout de blagnac_area et du code qui va avec

--- src/osmosis_ros/src/DetectionModules/checkProhibitedArea.py
+++ src/osmosis_ros/src/DetectionModules/checkProhibitedArea.py
@@ -5,6 +5,8 @@ from std_msgs.msg import Bool
 from geometry_msgs.msg import Pose2D
 import matplotlib.image as mpimg
 
+fault_injection=False
+
 def drive():
 
     if len(image)>0 and len(image[0])>0 :
@@ -16,27 +18,43 @@ def poseCallback(data):
     # Converting pose in pixels
     X=int( (80+float(data.x)) * (828/160) * 1.04)
     Y=int( (80-float(data.y)) * (740/160) * 1.15)
- 
-    
+
+
     if int(image[Y][X][0]) > 250 :
 	print("Couleur :", int(image[Y][X][0]), image[Y][X][1], image[Y][X][2], "TRUE")
         pub.publish(True)
 
     else :
- 	print("Couleur :", int(image[Y][X][0]), image[Y][X][1], image[Y][X][2], "FALSE")
-        pub.publish(False)
+        print("Couleur :", int(image[Y][X][0]), image[Y][X][1], image[Y][X][2], "FALSE")
+        print(fault_injection)
+        if fault_injection==True:
+            pub.publish(True)
+            print("saltations")
+        else:
+            pub.publish(False)
+
+def activationCallback(data):
+    global fault_injection
+    fault_injection=data.data
+    print(fault_injection)
+
 
 rospack = rospkg.RosPack()
 path = rospack.get_path('osmosis_control')
-path += "/../../Annexe/Affichage_graphe/blagnac_area.jpg"
+path += "/../../Appendix/Display_graph/blagnac_area.jpg"
 image = mpimg.imread(path)
 rospy.init_node('checkArea', anonymous=True)
 rospy.Subscriber("/pose", Pose2D, poseCallback)
 pub = rospy.Publisher('inProhibitedArea', Bool, queue_size=10)
+
+
+
+#fault injection
+rospy.Subscriber("/activate_DM1", Bool, activationCallback)
+
 
 if __name__ == '__main__':
     try:
         drive()
     except rospy.ROSInterruptException:
         pass
-
